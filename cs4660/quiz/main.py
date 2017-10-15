c373ae899d0790a33e714ac61ce634d4e143594e
yhoang001 <yhoang001@yahoo.com>
2017-10-15 16:34:37 -0700

    working Quiz 2

--- cs4660/quiz/main.py
+++ cs4660/quiz/main.py
@@ -26,7 +26,7 @@ def get_state(room_id):
     """
     get the room by its id and its neighbor
     """
-    
+
     body = {'id': room_id}
     return __json_request(GET_STATE_URL, body)
 
@@ -59,26 +59,70 @@ def bfs(start, finish):
     N = ((None, None))
     q = Queue()
     q.put((start, None))
+    c = 0
 
     while (q.qsize() > 0):
         i = q.get()
         for room in get_state(i[0]['id'])['neighbors']:
             if room not in nodes:
+                c += 1
                 if room['id'] == finish['id']:
-                    print("reached")
+                    if q.empty():
+                        return False
+                    print("\nBFS founded after", c)
                     N = i
-
-                q.put((room, i))
+                q.put_nowait((room, i))
                 nodes.append(room)
-
     while (N[1] is not None):
         moves.append(transition_state(N[1][0]['id'], (N[0])['id']))
         N = N[1]
-
     moves.reverse()
-    moves.append(transition_state(moves[len(moves) - 1]['id'], finish['id']))
-
+    t_state = transition_state(moves[len(moves) - 1]['id'], finish['id'])
+    moves.append(t_state)
     return moves
+
+def dijkstra(start, finish):
+    distance = {}
+    parent = {}
+    edges = {}
+    # print(distance_of[start['id']])
+    distance[start['id']] = 0
+    q = PriorityQueue()
+    q.put((0, start['id']))
+    visited = []
+    c = 0
+    while (q.qsize() > 0):
+        u = get_state(q.get()[1])
+        U = u['id']
+        visited.append(u['id'])
+        for i in u['neighbors']:
+            I = i['id']
+            edge = transition_state(U, I)
+            weight = distance[U] + edge['event']['effect']
+            if i['id'] not in visited and (I not in distance or weight > distance[I]):
+                c += 1
+                q.put((-weight, I))
+                distance[I] = weight
+                parent[I] = U
+                edges[I] = edge
+    moves = []
+    print("\nDijksta founded after", c)
+    while finish['id'] in parent:
+        moves.append(edges[finish['id']])
+        finish['id'] = parent[finish['id']]
+    moves.reverse()
+    return moves
+
+def print_actions(actions, init_id):
+    prev_id = init_id
+    total = 0
+    for i in range(len(actions)):
+        prev_node = get_state(prev_id)
+        next_id = actions[i]['id']
+        total += actions[i]['event']['effect']
+        print("%s(%s):%s(%s):%i" % (prev_node['location']['name'], prev_id, actions[i]['action'], actions[i]['id'], actions[i]['event']['effect']))
+        prev_id = next_id
+    print("\nTotal HP: %i" % total)
 
 if __name__ == "__main__":
     # Your code starts here
@@ -86,7 +130,39 @@ if __name__ == "__main__":
     dest_room = get_state('f1f131f647621a4be7c71292e79613f9')
     # print(empty_room,"check")
     # print(transition_state(empty_room['id'], empty_room['neighbors'][0]['id']))
-    bfs_result = bfs(empty_room, dest_room)
-    for move in bfs_result:
-        print(move)
+    bfs_action = bfs(empty_room, dest_room)
+    hp_bfs = 0
+    last_bfs = 0
+    print(empty_room['location']['name'], empty_room['id'])
+    for i in  bfs_action:
+        hp_bfs += i['event']['effect']
+        # print(move)
+        # move = get_state(bfs_action[i]['id'])
+        # last = empty_room
+        # next = bfs_action[i]['id']
+        print(i['action'], i['id'])
+        # print(i['action'],i['id'])
+        # last = next
+    print('\nHP: ', hp_bfs)
+
+    dij = dijkstra(empty_room, dest_room)
+    hp_dij = 0
+    for i in dij:
+        hp_dij += i['event']['effect']
+        print(i['action'], i['id'])
+    print('\nHP: ', hp_dij)
+    print('\n')
+    # dijkstra_result = dijkstra(empty_room, dest_room)
+    # d_hp = 0
+    # for u in dijkstra_result:
+    #     d_hp += u['event']['effect']
+    #     print(u)
+    # print('HP: ', d_hp)
+    # actions = dijkstra(empty_room, dest_room)
+    # actions = dijkstra('7f3dc077574c013d98b2de8f735058b4', 'f1f131f647621a4be7c71292e79613f9')
+    # for u in actions:
+    #     print(u)
+    # print("\nDijkstra Path:")
+    # print_actions(actions, '7f3dc077574c013d98b2de8f735058b4')
+    # print(actions)
 
